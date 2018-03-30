5696a8b86340c92b6c452ec94d1b076ff9f9aaa6
Andy <mindovermiles262@gmail.com>
2018-03-30 08:56:12 -0700

    added user change on click

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
 
+// -----  PERSON FACTORY  -----
 const personFactory = (name, marker, playerNumber) => {
   const sayHello  = () => {
     console.log(`Hello my name is ${name}`);
-  }
+  };
 
   const displayPerson = () => {
     document.querySelector(`#player${playerNumber}`).innerText = `${name}, ${marker}`
-  }
+  };
 
   displayPerson();
   
-  return { name, marker, sayHello, displayPerson }
-}
+  return { name, marker, sayHello, displayPerson };
+};
 
-const writeBox = (box, marker) => {
-  box.innerText = marker;
-}
 
-container.addEventListener('click', (event) => {
-  const target = event.target;
-  if (target.className === "box") {
-    writeBox(target, "x")
-  }
-});
 
-let hugo = personFactory("Hugo", "X", 1)
-let mark = personFactory("Mark", "O", 2)
+// -----  GAME FACTORY  -----
+const gameFactory = (player1, player2) => {
+  let currentPlayer = player1;
+  let gameArray = ["", "", "", "", "", "", "", "", ""];
+
+  const writeBox = (box, marker) => {
+    box.innerText = marker;
+  };
+
+  const switchPlayer = (currentPlayer) => {
+    if (currentPlayer === player1) {
+      return player2;
+    } else {
+      return player1;
+    };
+  };
+
+  container.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.className === "box") {
+      writeBox(target, currentPlayer.marker);
+      currentPlayer = switchPlayer(currentPlayer);
+    };
+  });
+};
+
+
+
+// ----- INIT GAME -----
+let hugo = personFactory("Hugo", "X", 1);
+let mark = personFactory("Mark", "O", 2);
+
+gameFactory(hugo, mark);
